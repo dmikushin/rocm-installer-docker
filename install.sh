b5180a30b1193e9e8b715921e8c8f51a52e28ab2
Dmitry Mikushin <dmitry@kernelgen.org>
2023-02-16 14:44:05 +0000

    Fixing bugs, adding alternative URL for Packages

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ex
 if ! grep -q "${ROCM_VERSION}" "/.installed"; then
+    wget https://repo.radeon.com/rocm/apt/${ROCM_VERSION}/dists/ubuntu/main/binary-amd64/Packages ||
+    wget https://repo.radeon.com/rocm/apt/${ROCM_VERSION}/dists/$(lsb_release -cs)/main/binary-amd64/Packages
     curl -fsSL http://repo.radeon.com/rocm/rocm.gpg.key | gpg --dearmor -o /usr/share/keyrings/rocm-archive-keyring.gpg &&
     ROCM_REPO=https://repo.radeon.com/rocm/apt/${ROCM_VERSION}/ \
     sh -c 'echo deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/rocm-archive-keyring.gpg] $ROCM_REPO ubuntu main > /etc/apt/sources.list.d/rocm.list' &&
     apt update &&
-    wget https://repo.radeon.com/rocm/apt/${ROCM_VERSION}/dists/focal/main/binary-amd64/Packages &&
     apt install -y --no-install-recommends \
         $(cat Packages | grep "Package: " | sed "s/Package: //g" | sed -e "s/^mi.*//g" | sed -e "s/${ROCM_VERSION}$//g" | sed "s/.*nvidia.*//g" | sed "s/.*sharp.*//g" | sed "s/rocm-dkms//g" | sort | uniq | xargs) &&
     echo "${ROCM_VERSION}" >>/.installed
--- rocm-install.sh
+++ rocm-install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -ex
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ROCM_VERSION>"
-    echo "Example: $0 5.3.3"
-    exit 1
+	echo "Usage: $0 <ROCM_VERSION>"
+	echo "Example: $0 5.3.3"
+	exit 1
 fi
 ROCM_VERSION=$1
 docker build -t rocm-${ROCM_VERSION} .
 docker run --rm -it \
-        -e ROCM_VERSION=${ROCM_VERSION} \
-	-v /opt/rocm-${ROCM_VERSION}:/opt/rocm-${ROCM_VERSION}
+	-e ROCM_VERSION=${ROCM_VERSION} \
+	-v /opt/rocm-${ROCM_VERSION}:/opt/rocm-${ROCM_VERSION} \
 	-v $(pwd)/.installed:/.installed \
-        amdgpu-${AMDGPU_VERSION} \
-        bash /install.sh
+	rocm-${ROCM_VERSION} \
+	bash /install.sh
+
